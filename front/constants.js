6f9bc3a1f94ebbf73f914eb71769b6d0af1dac72
Alexis <alexis.archer44@gmail.com>
2021-11-26 06:35:29 +0100

    fix env2

--- front/constants.js
+++ front/constants.js
@@ -1,19 +1,20 @@
 
 const navigationStructure = [
-    {
-        nav: "admin",
-        link: '/Admin'
-    },
-    {
-        nav: "nav2",
-        link: '/Admin2'
-
-    },
-    {
-        nav: "nav3",
-        link: '/Admin3'
-
-    }
+  {
+    nav: "admin",
+    link: '/Admin'
+  },
+  {
+    nav: "nav2",
+    link: '/contact'
+  },
+  {
+    link: '/contact/tamère'
+  },
+  {
+    nav: "nav3",
+    link: '/Admin3'
+  }
 ]
 
 export default navigationStructure
