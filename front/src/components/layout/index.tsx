2b417244ff598b493caf3d5b1166ad2476d2828f
Alexis <alexis.archer44@gmail.com>
2022-03-31 18:58:09 +0200

    fix

--- front/src/components/layout/index.tsx
+++ front/src/components/layout/index.tsx
@@ -1,22 +1,10 @@
-import {
-  Card,
-} from '@mui/material';
-import React, {
-  useRef,
-} from 'react';
-
-import { i18n } from '@lingui/core';
-import { t } from '@lingui/macro';
-import Image from 'next/image';
-import bgHome from '../../assets/bgHome.webp';
+import React from 'react';
 
 import { NAVIGATION_STRUCTURE } from '../../constants';
 import Header  from './Header';
 import Footer from './Footer';
 
 function Layout({ children }) {
-  const cardComponentRef = useRef < HTMLDivElement >(null);
-
   return (
     <div className="container_GlobalLayoutPage">
       <Header navigationStructure={NAVIGATION_STRUCTURE} />
