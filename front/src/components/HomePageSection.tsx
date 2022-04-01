6f2ee8e1fa26392e20a5689d201335716d70941d
Alexis <alexis.archer44@gmail.com>
2022-04-01 12:42:33 +0200

    homepageLayoutOK

---
+++ front/src/components/HomePageSection.tsx
@@ -0,0 +1,47 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable max-len */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Grid } from '@mui/material';
+import React from 'react';
+import { t } from '@lingui/macro';
+import { i18n } from '@lingui/core';
+import Image from 'next/image';
+import styles from '../styles/Home.module.scss';
+
+export interface section{
+  imageSrc:string,
+  title: string,
+  text:string,
+  index?:number,
+}
+function HomePageSection({
+  imageSrc = '', title = '', text = '', index,
+}:section) {
+  return (
+
+    <section className={styles?.sectionContainer}>
+      <Grid container>
+        <Grid className={styles.contentContainer} md={8} xs={12}>
+          <h2>
+            {i18n._(/* i18n: MainPageTItle1/2 */ t`${title}`)}
+          </h2>
+          <p>
+            {text}
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <a>Learn More</a>
+        </Grid>
+        <Grid xs={12} md={4} className={styles.imageContainer} style={{ order: index % 2 > 0 ? -1 : '' }}>
+          <Image
+            alt="bgHome"
+            src={imageSrc}
+            layout="fill"
+            className={styles.homeBackground}
+          />
+        </Grid>
+      </Grid>
+    </section>
+  );
+}
+
+export default HomePageSection;
--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-shadow */
 /* eslint-disable jsx-a11y/media-has-caption */
 /* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
@@ -10,15 +11,31 @@ import { GetStaticProps } from 'next';
 import { t } from '@lingui/macro';
 import { i18n } from '@lingui/core';
 import loadTranslation from '@utils/loadTranslation';
-import { Card } from '@mui/material';
+import { Card, Grid } from '@mui/material';
 import ReactPlayer from 'react-player';
 import Image from 'next/image';
+import HomePageSection, { section } from '@components/HomePageSection';
 import Layout from '../components/layout';
 import styles from '../styles/Home.module.scss';
 
 function Home() {
   const cardComponentRef = useRef < HTMLDivElement >(null);
-
+  const sectionsToDisply :section[] = [{
+    text: 'hoho',
+    title: 'we are experts',
+    imageSrc: '/bgHome.webp',
+  },
+  {
+    text: 'hoho',
+    title: 'we are experts',
+    imageSrc: '/bgBE.jpg',
+  },
+  {
+    text: 'haha',
+    title: 'we are experts',
+    imageSrc: '/bgHistory.webp',
+  },
+  ];
   return (
     <Layout>
       <div className={styles.pageContainer}>
@@ -34,69 +51,8 @@ function Home() {
           />
         </div>
 
-        <section className={styles?.sectionContainer}>
+        { sectionsToDisply.map((section, index) => <HomePageSection index={index} key={section.title} {...section} />) }
 
-          <div className={styles.contentContainer}>
-            <h2>
-              {i18n._(/* i18n: MainPageTItle1/2 */ t`Half a century of expertise`)}
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a>Learn More</a>
-          </div>
-          <div className={styles.imageContainer}>
-            <Image
-              alt="bgHome"
-              src="/bgHistory.webp"
-              layout="fill"
-              className={styles.homeBackground}
-            />
-
-          </div>
-        </section>
-
-        <section className={styles?.sectionContainer}>
-          <div className={styles.imageContainer}>
-            <Image
-              alt="bgHome"
-              src="/bgBE.jpg"
-              layout="fill"
-              className={styles.homeBackground}
-            />
-          </div>
-          <div className={styles.contentContainer}>
-            <h2>
-              {i18n._(/* i18n: MainPageTItle1/2 */ t`Half a century of expertise`)}
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a>Learn More</a>
-          </div>
-        </section>
-
-        <section className={styles?.sectionContainer}>
-
-          <div className={styles.contentContainer}>
-            <h2>
-              {i18n._(/* i18n: MainPageTItle1/2 */ t`Half a century of expertise`)}
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-            <a>Learn More</a>
-          </div>
-          <div className={styles.imageContainer}>
-            <Image
-              alt="bgHome"
-              src="/bgHome.webp"
-              layout="fill"
-              className={styles.homeBackground}
-            />
-
-          </div>
-        </section>
       </div>
     </Layout>
   );
