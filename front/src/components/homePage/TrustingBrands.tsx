6e62b007c2e45ff623a139bd7172bda637393fda
Alexis <alexis.archer44@gmail.com>
2022-04-06 17:58:36 +0200

    add logo container

--- front/src/components/homePage/TrustingBrands.tsx
+++ front/src/components/homePage/TrustingBrands.tsx
@@ -5,11 +5,11 @@ import styles from '../../styles/Home.module.scss';
 
 function TrustingBrands() {
   return (
-    <div className={styles.sectionContainer}>
+    <div className={`${styles.sectionContainer} ${styles.trustingBrands}`}>
       <h2>
         {i18n._(/* i18n: trustingBrands Title */ t`ILS NOUS FONT CONFIANCE`)}
       </h2>
-      <div>
+      <div className={styles.brandsList}>
         {[1, 2, 3, 4, 5].map((x) => <div key={`AAA${x}`}>ff</div>)}
       </div>
     </div>
