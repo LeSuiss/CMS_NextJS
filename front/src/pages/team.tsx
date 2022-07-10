1b2f8a6094125fadcbbf46fde6ee9b8ef655fd47
Alexis <alexis.archer44@gmail.com>
2022-07-10 23:42:30 +0200

    k

--- front/src/pages/team.tsx
+++ front/src/pages/team.tsx
@@ -52,9 +52,9 @@ export default team
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const translation = await loadTranslation(ctx.locale!)
 
-  const logoList = path.join(process.cwd(), '/public/medias/logo')
-  const brandsList = fs.readdirSync(logoList)
-  const employees = await getEmployees(ctx.locale)
+  const logoList = path.join(process.cwd(), '/public/medias/logo') ?? []
+  const brandsList = fs.readdirSync(logoList) ?? []
+  const employees = await getEmployees(ctx.locale) ?? []
 
   return {
     props: {
