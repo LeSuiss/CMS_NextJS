984e86fa1935335537c1c15196b8e061676c2bb2
Alexis <alexis.archer44@gmail.com>
2022-07-17 16:48:22 +0200

    k

--- front/src/components/homePage/config.ts
+++ front/src/components/homePage/config.ts
@@ -18,14 +18,14 @@ export const sectionsData = [
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit.',
     title: 'Innovation is the Key',
     imageSrc: '/medias/bgBE.webp',
-    textLink: 'Discover our experts',
+    linkText: 'Discover our experts',
     linkHref: '/innovation',
   },
   {
     text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     title: 'Our Products',
     imageSrc: '/medias/bgHistory.webp',
-    textLink: 'Discover our experts',
+    linkText: 'Discover our experts',
     linkHref: '/products',
   },
 ]
