4c76bc2968e56c48a7285a08558fc20e1bc747a4
Alexis <alexis.archer44@gmail.com>
2023-04-05 16:52:12 +0100

    k

--- front/next.config.js
+++ front/next.config.js
@@ -3,6 +3,25 @@
 const path = require('path');
 
 const nextConfig = {
+  async headers() {
+    return [{
+      source: "/api/:path*",
+      headers: [{
+        key: "Access-Control-Allow-Credentials",
+        value: "true"
+      }, {
+        key: "Access-Control-Allow-Origin",
+        value: "*"
+      }, {
+        key: "Access-Control-Allow-Methods",
+        value: "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+      }, {
+        key: "Access-Control-Allow-Headers",
+        value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+      },]
+    }]
+
+  },
   reactStrictMode: true,
   env: {
     CI: process.env.CI,
@@ -17,19 +36,19 @@ const nextConfig = {
 
   images: {
     domains: ['res.cloudinary.com', "cloudflare-ipfs.com"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
+    remotePatterns: [{
+      protocol: "https",
+      hostname: "**",
+    },],
   },
   i18n: {
     locales: ['en', 'fr'],
     defaultLocale: 'fr',
   },
 
-  webpack: (config, { isServer }) => {
+  webpack: (config, {
+    isServer
+  }) => {
     config.module.rules.push({
       test: /\.po/,
       use: ['@lingui/loader'],
